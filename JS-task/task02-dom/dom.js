8618e4f6323660310bf0367f9346a5878f133f7d
Fatrai1 <fatrai.cb@gmail.com>
2021-04-29 20:25:53 +0200

    js

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,11 +1,17 @@
 function changeElements() {
-    const examples = document.querySelectorAll('.example');
-    for (let i = 0; i < examples.length; i++) {
-        examples[i].style.backgroundColor = 'red';
-        examples[i].style.fontStyle = 'italic';
-    }
+   document.querySelectorAll('.example').forEach( e => {
+        e.style.backgroundColor = 'red';
+        e.style.fontStyle = 'italic';
+   });
 }
 
 
 
 export { changeElements };
+
+
+// const examples = document.querySelectorAll('.example');
+// for (let i = 0; i < examples.length; i++) {
+//     examples[i].style.backgroundColor = 'red';
+//     examples[i].style.fontStyle = 'italic';
+// }
